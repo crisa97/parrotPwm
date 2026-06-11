e6caa88d22da0d60dd8e82d374e3fe13caab037b
crisa <crisa@localhost.localdomain>
2026-06-11 09:53:51 -0500

    configuracion de volumen y full scream

--- CONFIGS/config/bspwm/scripts/exit-fullscreen.sh
+++ CONFIGS/config/bspwm/scripts/exit-fullscreen.sh
@@ -3,6 +3,4 @@ WID=$(xdotool getactivewindow)
 STATE=$(bspc query -T -n "$WID" | grep -oP '"state":\s*"\K[^"]+')
 if [ "$STATE" = "fullscreen" ]; then
     bspc node "$WID" -t tiled
-else
-    bspc node "$WID" -t fullscreen
 fi
